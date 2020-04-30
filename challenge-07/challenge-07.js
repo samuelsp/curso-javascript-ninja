03902ea34df3fc78114bcba826d60c9691397f07
Samuel Silva Pereira Da Cunha <samuelspcunha@gmail.com>
2020-04-30 09:37:11 -0300

    challenge07 update!

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -61,7 +61,7 @@ console.log( 'Números ímpares entre 10 e 20:' );
 var num = 10;
 while( num <= 20 ) {
     if( num % 2 !== 0 )
-        console.log(num);
+        console.log( num );
     num++;
 }
 
